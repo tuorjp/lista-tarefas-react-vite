e5fe34464355cf91cd708eb88472ed8c23df3198
JP <jplmarotinho22@gmail.com>
2023-04-15 14:48:23 -0300

    feat: :construction: função adicionar tarefa

--- todo_list/src/App.tsx
+++ todo_list/src/App.tsx
@@ -3,14 +3,23 @@ import * as C from "./styles/App.styles"
 import {Item} from './types/Item'
 import { GlobalStyle } from './styles/global'
 import {ListItem} from './components/ListItem'
+import {AddArea} from './components/AddArea'
 
 function App() {
 
   //O state recebe array de items, por isso a tipagem. Para exemplo vamos iniciar o estado com dois items
-  const [list, setList] = useState<Item[]>([
-    {id: 1, name: 'Estudar useContext', done: false},
-    {id:2, name: 'Estudar CSS Grid', done: false},
-  ])
+  const [list, setList] = useState<Item[]>([])
+
+  //função que adiciona uma nova tarefa na lista
+  function handleAddTask(taskName: string) {
+    let newList = [...list]
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    })
+    setList(newList)
+  }
 
   return(
     <>
@@ -18,7 +27,7 @@ function App() {
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        {/*Área de adicionar nova tarefa*/}
+        <AddArea onEnter={handleAddTask}/>
 
         {list.map((item, index) => {
           return(
---
+++ todo_list/src/components/AddArea/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+
+    .img {
+        margin-right: 5px;
+    }
+
+    input {
+        border: none;
+        background: transparent;
+        color: #fff;
+        outline: 0;
+        font-size: 18px;
+        flex: 1;
+    }
+`
