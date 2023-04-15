05c49396e382eb4eb5c70e3e65bd086adb760974
JP <jplmarotinho22@gmail.com>
2023-04-15 12:51:42 -0300

    feat: :sparkles: implementando lógica checked

--- todo_list/src/components/ListItem/styles.ts
+++ todo_list/src/components/ListItem/styles.ts
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+type ContainerInputProps = {
+    done: boolean
+}
+
+export const Container = styled.div(({done}: ContainerInputProps) => (
+    `
     display: flex;
     background-color: #20212C;
-    padding: 10px;
+    padding: 10px 10px;
     border-radius: 10px;
-    margin-bottom: 10px;
+    margin: 10px;
     align-items: center;
 
     input {
+        :hover{
+            cursor: pointer;
+        }
         width: 25px;
         height: 25px;
         margin-right: 5px;
@@ -16,5 +24,7 @@ export const Container = styled.div`
 
     label {
         color: #CCC;
+        text-decoration: ${done ? 'line-through' : 'initial'};
     }
 `
+))
