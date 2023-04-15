bc2c292b0205d8e3edceff3b0651ea01b650a833
JP <jplmarotinho22@gmail.com>
2023-04-15 11:51:15 -0300

    feat: :tada: lista de tarefas
    items apresentados dinamicamente usando useState e map( )

---
+++ todo_list/src/App.tsx
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import * as C from "./App.styles"
+import {Item} from './types/Item'
+
+function App() {
+
+  //O state recebe array de items, por isso a tipagem. Para exemplo vamos iniciar o estado com dois items
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Estudar useContext', done: false},
+    {id:2, name: 'Estudar CSS Grid', done: false},
+  ])
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/*Área de adicionar nova tarefa*/}
+
+        {list.map((item, index) => {
+          return(
+            <div>{item.name}</div>
+          )
+        })}
+
+      </C.Area>
+    </C.Container>
+  )
+}
+
+export default App
